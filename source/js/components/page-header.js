ebc0ffbb9023e0d3b1dbb2ff957c00c1cb864925
srg <skabass@yandex.ru>
2020-09-23 01:19:29 +0300

    Добавляет запрет скролла при открытом меню

--- source/js/components/page-header.js
+++ source/js/components/page-header.js
@@ -3,6 +3,7 @@
 (function () {
   var BREAKPOINT_MOBILE = window.utils.BREAKPOINT_MOBILE;
 
+  var body = document.querySelector('body');
   var header = document.querySelector('.page-header');
   var btnOpen = header.querySelector('.page-header__btn-open');
   var btnClose = header.querySelector('.page-header__btn-close');
@@ -10,10 +11,12 @@
 
   var closeMenu = function () {
     menu.classList.remove('page-header__menu-wrapper--open');
+    body.classList.remove('overflow-hidden');
   };
 
   btnOpen.addEventListener('click', function () {
     menu.classList.add('page-header__menu-wrapper--open');
+    body.classList.add('overflow-hidden');
   });
 
   btnClose.addEventListener('click', function () {
