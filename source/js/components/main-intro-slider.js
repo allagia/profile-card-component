2fb0f6a27476365ca783583df21dfce075e80f32
vladykod <Vladyko@htmacademy.ru>
2020-09-28 03:43:17 +0300

    слайдер с главной страницы

--- source/js/components/main-intro-slider.js
+++ source/js/components/main-intro-slider.js
@@ -2,13 +2,15 @@
 
 (function () {
 
-  var swiper1 = new Swiper('.main-slider1', {
+  var parentSlider = document.querySelector('.main-slider1');
+  var nextSlide = document.querySelector('.main-slider2');
+  var nextSlideItems = nextSlide.querySelectorAll('.swiper-slide');
+
+  var swiper1 = new Swiper(parentSlider, {
     slidesPerView: 1,
-    centerInsufficientSlides: true,
     loop: true,
-    autoHeight: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.main-sliders__pagination1',
       type: 'fraction',
       formatFractionCurrent: function (number) {
         if (number < 10) {
@@ -24,14 +26,20 @@
       },
     },
     navigation: {
-      nextEl: '.main-sliders__button-prev1',
-      prevEl: '.main-sliders__button-next1',
+      prevEl: '.main-sliders__button-prev1',
+      nextEl: '.main-sliders__button-next1',
     },
-    thumbs: {
-      swiper: swiper2,
-    }
+    on: {
+      slideChange: function () {
+        nextSlideItems.forEach(function(el) {
+          el.classList.remove('active');
+        });
+        nextSlideItems[this.realIndex].classList.add('active');
+      },
+    },
   });
 
+  /*
   var swiper2 = new Swiper('.main-slider2', {
     slidesPerView: 1,
     centerInsufficientSlides: true,
@@ -61,6 +69,6 @@
       swiper: swiper1,
     }
   });
-
+*/
 
 })();
