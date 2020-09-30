d3db2a8fa82189858aec26ac795cd5c5d5c1aa96
Alla Panova <alla.mixalvna@gmail.com>
2020-09-30 16:08:40 +0300

    добавляет аккордион

--- build/js/main.js
+++ build/js/main.js
@@ -376,3 +376,74 @@
 
 })();
 
+'use strict';
+
+(function () {
+  var acc = document.querySelectorAll('.card-accordion__item');
+  var paddingBottom = 36;
+
+  if (acc) {
+    // acc.classList.add('accordion--js');
+
+    for (var i = 0; i < acc.length; i++) {
+      acc[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        var panel = this.nextElementSibling;
+
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.height = panel.scrollHeight + paddingBottom + 'px';
+        }
+      });
+    }
+  }
+
+
+  // var accordion = document.querySelector('.accordion');
+
+  // if (accordion) {
+  //   accordion.classList.add('accordion--js');
+
+  //   var titles = accordion.querySelectorAll('.accordion__tab-title');
+
+  //   var onTitleClick = function (evt) {
+  //     var rtime;
+  //     var timeout = false;
+  //     var delta = 100;
+
+  //     window.addEventListener('resize', (function () {
+  //       rtime = new Date();
+  //       if (timeout === false) {
+  //         timeout = true;
+  //         setTimeout(resizeend, delta);
+  //       }
+  //     }), false);
+
+  //     function resizeend() {
+  //       if (new Date() - rtime < delta) {
+  //         setTimeout(resizeend, delta);
+  //       } else {
+  //         timeout = false;
+  //         var tabContent = evt.target.parentElement.querySelector('.accordion__tab-content');
+  //         var tabContentHeight = tabContent.querySelector('p').scrollHeight + 'px';
+  //         tabContent.style.height = (tabContent.offsetHeight === 0) ? 0 : tabContentHeight;
+  //       }
+  //     }
+
+  //     var tabContent = evt.target.parentElement.querySelector('.accordion__tab-content');
+  //     var tabContentHeight = tabContent.querySelector('p').scrollHeight + 'px';
+
+  //     evt.target.parentElement.classList.toggle('accordion__tab--opened');
+  //     tabContent.style.height = (tabContent.offsetHeight === 0) ? tabContentHeight : 0;
+  //   };
+
+  //   var onDocumentKeyDown = function (evt) {
+  //     if (evt.keyCode === window.constant.KeyCode.ENTER && evt.target.classList.contains('accordion__tab-title')) {
+  //       evt.preventDefault();
+  //       onTitleClick(evt);
+  //     }
+  //   };
+  // }
+})();
+
